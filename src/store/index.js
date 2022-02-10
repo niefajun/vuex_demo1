5d79aa79da270cd0bcaa7359f9ed7b2621782a7d
niefajun <fajun.nie@xdf.cn>
2022-02-10 21:18:31 +0800

    1.定义全局共享数据count 2.使用this.$store.state.全局数据名称来获取全局共享数据

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,11 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // 创建store数据源，提供唯一公共数据
   state: {
+    count: 0 // 对应计数器值
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  mutations: {},
+  actions: {},
+  modules: {}
 })
