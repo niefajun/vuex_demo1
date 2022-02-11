838e380bc2d57c7dcc9f1c9db4c0be0c16ba4159
niefajun <1422872995@qq.com>
2022-02-11 17:03:08 +0800

    使用mapActions映射action方法补充

--- src/components/Subtraction.vue
+++ src/components/Subtraction.vue
@@ -3,11 +3,13 @@
     <h3>当前最新的count值为:{{ count }}</h3>
     <button @click='btnHandler3'>-1</button>
     <button @click='btnHandler4'>-N</button>
+    <button @click='btnHandler5'>-1 async</button>
+    <button @click='btnHandler6'>-N async</button>
   </div>
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex'
+import { mapState, mapMutations, mapActions } from 'vuex'
 
 export default {
   data () {
@@ -19,11 +21,18 @@ export default {
   methods: {
     // 将全局mutation方法映射为当前组件的方法method
     ...mapMutations(['sub', 'subN']),
+    ...mapActions(['subAsync', 'subNAsync']),
     btnHandler3 () {
       this.sub()
     },
     btnHandler4  () {
       this.subN(3)
+    },
+    btnHandler5 () {
+      this.subAsync()
+    },
+    btnHandler6 () {
+      this.subNAsync(3)
     }
   }
 }
